e665da6d4b09267b77eea3040801a821eeb33f64
Dilin-am <dilinam91@gmail.com>
2023-09-05 02:58:59 +0530

    settimeout

--- src/views/Student.jsx
+++ src/views/Student.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import Main from "../layouts/Main";
 import { Row, Table ,Col} from "react-bootstrap";
 const Student = () => {
+    
+const [student,setStudents]= useState([]);
+setTimeout(() => {
+    setStudents([{firstname:"dilin",lastname:"am",mobile:"98954123",email:"dilin@gmail.com",city:"clt",degree:"yes",skill:["html","css","react"]},
+    {firstname:"bibin",lastname:"das",mobile:"98954123",email:"dilin@gmail.com",city:"knr",skill:"js",degree:"yes"}
+])
+}, 5000);
     return(<>
     <Main>
     <Row className="mt-5">
@@ -9,30 +17,31 @@ const Student = () => {
         <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
+          <th>Sl.no.</th>
           <th>First Name</th>
           <th>Last Name</th>
-          <th>Username</th>
+          <th>Mobile</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Degree</th>
+          <th>Skills</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+        {student.map((student,key)=>(
+        <tr key={key}>
+          <td>{key+1}</td>
+          <td>{student.firstname}</td>
+          <td>{student.lastname}</td>
+          <td>{student.mobile}</td>
+          <td>{student.email}</td>
+          <td>{student.city}</td>
+          <td>{student.degree}</td>
+          <td>{student.skill}</td>
+          
         </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        ))}
+        {student.length===0?(<tr className="text-center"><td colSpan="8">NO RECORDS FOUND</td></tr>):null}
       </tbody>
     </Table>
     </Col>
